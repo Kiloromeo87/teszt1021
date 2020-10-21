5c265bec9162d27a8c3cd13c8dd2459abe029432
Daniel <daniel.varsas.com>
2020-10-21 09:50:31 +0200

    feladat 3 létrehozva

---
+++ feladat3.js
@@ -0,0 +1,10 @@
+const tomb = [1, 2, 10, 100, 101, 102, 105];
+let paroscount = 0;
+
+let paroscount = (a) => {
+for ( let i = 0; i < tomb.legth; i++) {
+	if ( i % 2 === 0) {
+	paroscount++;
+}	
+}
+}
