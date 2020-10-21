46f2acb328882111238b893af4bbb3f289b22540
Daniel <daniel.varsas.com>
2020-10-21 09:40:53 +0200

    feladat 1 és 2

---
+++ feladat2.js
@@ -0,0 +1,13 @@
+let oszt3 = 0;
+let oszt5 = 0;
+
+for ( i = 0; i < 100; i++)
+	if ( i % 3 === 0); {
+	console.log('fizz');
+	oszt3 = true;	
+}
+	else if ( i % 5 === 0); {
+	console.log('buzz');}
+	oszt5 = true;}
+	else if ( oszt3 === true &&& oszt5 === true) { 
+	console.log('FizzBuzz');
